d1e30581fb5fcfda3f3b341d928dc3e07916d809
Paul M. Rodriguez <pmr@ruricolist.com>
2017-07-11 15:43:56 -0500

    Use iterate.

--- cmd.lisp
+++ cmd.lisp
@@ -1,21 +1,24 @@
 (cl:defpackage :overlord/cmd
-  (:use :cl :alexandria :serapeum)
+  (:use :cl :alexandria :serapeum :iterate)
+  (:shadowing-import-from :serapeum :collecting :summing :in)
   (:import-from :overlord/base :base)
   (:import-from :overlord/types :list-of :plist)
   (:export :cmd))
 (cl:in-package :overlord/cmd)
 
 (defun parse-cmd-args (args)
-  (loop for arg in args
-        if (stringp arg)
-          nconc (tokens arg) into tokens
-        else if (pathnamep arg)
-               collect (uiop:native-namestring arg) into tokens
-        else if (typep arg '(list-of string))
-               append arg into tokens
-        else if (typep arg 'plist)
-               append arg into plist
-        finally (return (values tokens plist))))
+  (iterate (for arg in args)
+    (typecase arg
+      (string
+       (appending (tokens arg) into tokens))
+      (pathname
+       (collect (uiop:native-namestring arg) into tokens))
+      (plist
+       (appending arg into plist))
+      ((list-of string)
+       (appending arg into tokens))
+      (t (error "Can't use ~a as a cmd argument." arg)))
+    (finally (return (values tokens plist)))))
 
 (defun cmd (&rest args)
   (multiple-value-bind (tokens plist) (parse-cmd-args args)
