e818a302ea43abd44151091b1a2ad9dd2dfa5ab5
Paul M. Rodriguez <pmr@ruricolist.com>
2017-10-27 14:53:02 -0500

    Simplify logging.

--- message.lisp
+++ message.lisp
@@ -5,31 +5,16 @@
   (:export
    :overlord-message
    :message
-   :*message-handler*
    :*message-stream*))
 (in-package :overlord/message)
 
 (define-global-state *message-stream*
-  (make-synonym-stream '*trace-output*)
+  (make-synonym-stream '*standard-output*)
   "The stream printed to by the default message handler.")
 
-(defun default-message-handler (msg)
-  (let ((stream *message-stream*)
-        (control (simple-condition-format-control msg))
-        (args    (simple-condition-format-arguments msg)))
+(defun message (control &rest args)
+  (let ((stream *message-stream*))
     (format stream "~&[Overlord] ~?~%" control args)))
-
-(define-global-state *message-handler* 'default-message-handler
-  "The current handler for message conditions.")
-(declaim (type (or function symbol) *message-handler*))
-
-(defcondition overlord-message (overlord-condition simple-condition)
-  ())
-
-(defun message (format-control &rest format-arguments)
-  (signal 'overlord-message
-          :format-control format-control
-          :format-arguments format-arguments))
 
 (define-compiler-macro message (&whole call format-control &rest format-arguments)
   (eif (not (stringp format-control)) call
