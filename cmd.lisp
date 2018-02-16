960e8ed8bb0fda2b9539fdee862169d5403e90d2
Paul M. Rodriguez <pmr@ruricolist.com>
2018-02-15 19:09:12 -0600

    Make `cmd` start processes in the thread-local current directory.

--- cmd.lisp
+++ cmd.lisp
@@ -1,9 +1,11 @@
 (cl:defpackage :overlord/cmd
   (:use :cl :alexandria :serapeum)
   (:shadowing-import-from :serapeum :collecting :summing :in)
-  (:import-from :overlord/base :base)
-  (:import-from :overlord/types :list-of :plist)
+  (:import-from :overlord/specials :use-threads?)
+  (:import-from :overlord/base :base :current-dir!)
+  (:import-from :overlord/types :list-of :plist :error*)
   (:import-from :overlord/message :*message-stream*)
+  (:import-from :uiop :os-windows-p)
   (:export :cmd))
 (cl:in-package :overlord/cmd)
 
@@ -15,14 +17,7 @@
         (string
          (qconc tokens (tokens arg)))
         (pathname
-         (let ((string (uiop:native-namestring arg)))
-           (when (string^= "-" string)
-             ;; Should we ignore the unsafe file names if `--' or
-             ;; `---' is already present in the list of tokens?
-             (cerror "Allow the unsafe file name"
-                     "File name ~a begins with a dash"
-                     string))
-           (enq string tokens)))
+         (enq (escape-pathname arg) tokens))
         (plist
          (qappend plist arg))
         ((list-of string)
@@ -31,10 +26,54 @@
     (values (qlist tokens)
             (qlist plist))))
 
+(defun escape-pathname (arg)
+  (lret ((string (uiop:native-namestring arg)))
+    (when (string^= "-" string)
+      ;; Should we ignore the unsafe file names if `--' or
+      ;; `---' is already present in the list of tokens?
+      (cerror "Allow the unsafe file name"
+              "File name ~a begins with a dash"
+              string))))
+
+(defun wrap-with-current-dir (tokens)
+  "Wrap TOKENS with the necessary code to run the process in a new directory.
+
+The OS-level current directory is per-process, not per thread. Using
+`chdir' could lead to race conditions. Instead, we arrange for the new
+process to change its own working directory."
+  (if (not (use-threads?)) tokens
+      (destructuring-bind (command . args) tokens
+        (let ((dir (escape-pathname (current-dir!))))
+          (if (not (os-windows-p))
+              `("/bin/sh"
+                "-c"
+                ;; Use Bernstein chaining; change to the directory in
+                ;; $1, shift, and exec the rest of the argument array.
+                ;; (If only there were a standard tool to do this, along
+                ;; the lines of chroot or su, so we wouldn't have to
+                ;; spin up a shell.)
+                "cd $1; shift; exec \"$@\""
+                ;; Terminate processing of shell options; everything
+                ;; after this is passed through.
+                "--"
+                ,dir
+                ,command
+                ,@args)
+              ;; This looks weird, but it actually works, because the
+              ;; Windows API to start a process is called with a
+              ;; string rather than an array. We still want to pass a
+              ;; list of arguments, though, rather than concatenating
+              ;; them ourselves, to evade responsibility for escaping.
+              `("cmd"
+                "/c"
+                "cd" ,dir
+                ;; Ampersand is the command separator.
+                "&" ,command ,@args))))))
+
 (defun cmd (&rest args)
   (multiple-value-bind (tokens plist) (parse-cmd-args args)
     (multiple-value-call #'uiop:run-program
-      tokens
+      (wrap-with-current-dir tokens)
       (values-list plist)
       :output t
       :error-output *message-stream*)))
