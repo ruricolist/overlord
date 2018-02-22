c0251e6aa91ce6f1ce05f613b724f9248061eec1
Paul M. Rodriguez <pmr@ruricolist.com>
2018-02-21 19:08:17 -0600

    Define a with-standard-input-syntax macro.
    For cases where we want to be able to debug the code within the macro
    without worrying about the printer control variables.

--- db.lisp
+++ db.lisp
@@ -163,6 +163,35 @@
           (copy-readtable fset::*fset-rereading-readtable*)))
     (local-time:enable-read-macros)))
 
+(def standard-input-syntax-vars
+  '(*package*
+    *read-base*
+    *read-default-float-format*
+    *read-eval*
+    *read-suppress*
+    *readtable*)
+  "Reader control variables bound by `with-standard-io-syntax'.")
+
+(def standard-input-syntax-values
+  (with-standard-io-syntax
+    (mapcar #'symbol-value standard-input-syntax-vars))
+  "Values of the standard reader control variables.")
+
+(defun call/standard-input-syntax (fn)
+  (progv standard-input-syntax-vars
+      standard-input-syntax-values
+    (funcall fn)))
+
+(defmacro with-standard-input-syntax (&body body)
+  "Like `with-standard-io-syntax', but only bind the variables that
+control the reader, not the printer.
+
+This may be preferable to using `with-standard-io-syntax' when loading
+data, as it will not effect how errors are printed, thus preserving
+debugging information."
+  (with-thunk (body)
+    `(call/standard-input-syntax ,body)))
+
 (defun call/standard-io-syntax (fn)
   "Like `with-standard-io-syntax', but if there is an error, unwind
 the stack so the error itself can be printed."
@@ -219,7 +248,7 @@ the stack so the error itself can be printed."
        :retry
          (restart-case
              (return-from log.load
-               (with-standard-io-syntax*
+               (with-standard-input-syntax
                  (let* ((*readtable* kv-readtable)
                         (records
                           (with-input-from-file (in log :element-type 'character)
