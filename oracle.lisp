5e34b96f7067fd701e167391346965860da44be7
Paul M. Rodriguez <pmr@ruricolist.com>
2018-09-26 17:25:18 -0500

    Better terminology for oracles.

--- oracle.lisp
+++ oracle.lisp
@@ -24,79 +24,81 @@
     :version-major-version)
   (:import-from :fset)
   (:export
-   :oracle :oracle-name
+   :oracle
+   :oracle-question
+   :oracle-answer
    :var-oracle
    :env-oracle
    :system-version-oracle
    :feature-oracle
    :dist-version-oracle
-   :function-oracle
-   :name-oracle))
+   :function-oracle))
 (in-package :overlord/oracle)
 
 ;;; TODO Would it be worthwhile to provide oracles for optimization
 ;;; qualities (speed, debug, safety)? You could get them through
 ;;; introspect-environment.
 
-(deftype oracle-value ()
+(deftype oracle-answer ()
   '(or
     number
     ;; NB boolean is a subtype of symbol.
     symbol
     string))
 
-(defgeneric oracle-value (oracle))
+(defgeneric oracle-answer (oracle))
 
 (defclass oracle ()
-  ((key :initarg :key
-        :reader oracle-name))
+  ((question :initarg :question
+             :reader oracle-question))
   (:documentation "Oracles let you depend on aspects of the Lisp or OS
 environment.
 
-When you depend on an oracle, Overlord remembers the value of the
-oracle at the time of the dependency \(or the lack of a value, if the
-oracle was unbound). If that value changes, then any targets that
+When you depend on an oracle, Overlord remembers the answer the oracle
+gave at the time of the dependency \(or the lack of an answer, if the
+oracle was unbound). If that answer changes, then any targets that
 depend on the oracle are considered out of date."))
 
-(defmethods oracle (self key)
+(defmethods oracle (self question)
   (:method make-load-form (self &optional env)
     (make-load-form-saving-slots self
-                                 :slot-names '(key)
+                                 :slot-names '(question)
                                  :environment env))
   (:method print-object (self stream)
     (format stream "~a~s"
             (read-eval-prefix self stream)
-            `(make-instance ',(class-name-of self) :key ,key)))
+            `(make-instance ',(class-name-of self) :question ,question)))
 
   (:method fset:compare (self (other oracle))
     (fset:compare-slots self other
                         #'class-name-of
-                        #'oracle-name))
+                        #'oracle-question))
   (:method target= (self (other oracle))
     (eql :equal (fset:compare self other)))
 
 
   (:method hash-target (self)
     (sxhash (make-load-form self)))
-  (:method oracle-value :around (self)
-    (assure oracle-value
+  (:method oracle-answer :around (self)
+    (assure oracle-answer
       (call-next-method)))
   (:method target-stamp (self)
-    (let ((value (oracle-value self)))
-      (if (stringp value)
-          (byte-array-to-hex-string (digest-string value))
-          (prin1-to-string (oracle-value self)))))
+    (let ((answer (oracle-answer self)))
+      (if (stringp answer)
+          (byte-array-to-hex-string (digest-string answer))
+          (prin1-to-string answer))))
   (:method target-node-label (self)
-    (fmt "oracle for ~a" key)))
+    (fmt "oracle for ~a" question)))
 
 
 ;;; Var oracles.
 
 (defclass var-oracle (oracle)
-  ((key :reader var-oracle.var
-        :initarg :var
-        :type delayed-symbol)
-   (sym :type symbol))
+  ((question :reader var-oracle.var
+             :initarg :var
+             :type delayed-symbol)
+   (sym :type symbol
+        :documentation "Cache for the resolved symbol."))
   (:default-initargs
    :var (required-argument :var))
   (:documentation "Oracle that wraps a special variable.
@@ -106,13 +108,13 @@ record the fact that it depends on some aspect of the compile time
 or read time environment (e.g. `*read-base*') and should be
 considered out of date if that changes."))
 
-(defmethods var-oracle (self (var key) sym)
+(defmethods var-oracle (self (var question) sym)
   (:method initialize-instance :after (self &key var)
     (setf var (delay-symbol var)))
   ;; Cache the symbol.
   (:method slot-unbound ((class t) self (slot-name (eql 'sym)))
     (setf sym (force-symbol var)))
-  (:method oracle-value (self)
+  (:method oracle-answer (self)
     (symbol-value sym))
   (:method target-exists? (self)
     (boundp sym))
@@ -124,8 +126,8 @@ considered out of date if that changes."))
         (delayed-symbol= var (var-oracle.var other))))))
 
 (defclass cl-var-oracle (oracle)
-  ((key :type symbol
-        :initarg :var))
+  ((question :type symbol
+             :initarg :var))
   (:default-initargs
    :var (required-argument :var))
   (:documentation
@@ -135,48 +137,48 @@ considered out of date if that changes."))
   (eql (symbol-package sym)
        (find-package :cl)))
 
-(defmethods cl-var-oracle (self (var key))
+(defmethods cl-var-oracle (self (var question))
   (:method initialize-instance :after (self &key)
     (assert (cl-sym? var)))
-  (:method oracle-value (self)
+  (:method oracle-answer (self)
     (symbol-value var))
   (:method target-exists? (self)
     (boundp var))
   (:method target= (self (other cl-var-oracle))
-    (eql var (slot-value other 'var))))
+    (eql var (oracle-question other))))
 
 (defclass name-oracle (oracle)
-  ((key :reader name-oracle.name))
+  ((question :reader name-oracle.name))
   (:documentation
    "Oracle that extracts a name from a value instead of recording the
-   value directly."))
+value directly. The question is also the answer."))
 
-(defmethods name-oracle (self (name key))
-  (:method oracle-value (self)
+(defmethods name-oracle (self (name question))
+  (:method oracle-answer (self)
     name)
   (:method target-exists? (self)
     t)
   (:method target= (self (other name-oracle))
-    (eql name (oracle-name other))))
+    (eql name (oracle-question other))))
 
 (defclass package-oracle (name-oracle)
-  ((key :type string
-        :reader package-oracle.name
-        :initform (package-name *package*)))
+  ((question :type string
+             :reader package-oracle.name
+             :initform (package-name *package*)))
   (:documentation "Oracle that wraps the current package."))
 
-(defmethods package-oracle (self (name key))
+(defmethods package-oracle (self (name question))
   (:method target= (self (other package-oracle))
     (string= name (package-oracle.name other))))
 
 (defclass readtable-oracle (name-oracle)
-  ((key :type delayed-symbol
-        :initform (delay-symbol (readtable-name *readtable*))))
+  ((question :type delayed-symbol
+             :initform (delay-symbol (readtable-name *readtable*))))
   (:documentation "Oracle that wraps the current readtable.
 
 A name is extracted using `named-readtable:readtable-name'."))
 
-(defmethods readtable-oracle (self (name key))
+(defmethods readtable-oracle (self (name question))
   (:method target= (self (other readtable-oracle))
     (delayed-symbol= name (name-oracle.name other))))
 
@@ -194,19 +196,19 @@ A name is extracted using `named-readtable:readtable-name'."))
 ;;; Environment oracles.
 
 (defclass env-oracle (oracle)
-  ((key :initarg :name
-        :type string
-        :reader env-oracle.name))
+  ((question :initarg :name
+             :type string
+             :reader env-oracle.name))
   (:default-initargs
    :name (required-argument :name))
   (:documentation "Oracle that wraps an environment variable."))
 
-(defmethods env-oracle (self (name key))
-  (:method oracle-value (self)
+(defmethods env-oracle (self (name question))
+  (:method oracle-answer (self)
     (uiop:getenv name))
   (:method target-stamp (self)
     (~> self
-        oracle-value
+        oracle-answer
         digest-string
         byte-array-to-hex-string))
   (:method target-exists? (self)
@@ -222,23 +224,23 @@ A name is extracted using `named-readtable:readtable-name'."))
 ;;; System version oracles.
 
 (defclass system-version-oracle (oracle)
-  ((key :initarg :name
-        :reader system-version-oracle.system-name
-        :type string))
+  ((question :initarg :name
+             :reader system-version-oracle.system-name
+             :type string))
   (:documentation "Using a system version oracle, you can depend on
 the major version of an ASDF system.
 
 Note that if the system is not known to ASDF, then the version
 recorded is simply nil."))
 
-(defmethods system-version-oracle (self (name key))
+(defmethods system-version-oracle (self (name question))
   (:method target= (self (other system-version-oracle))
     (let ((other-name
             (system-version-oracle.system-name other)))
       (string-equal name other-name)))
   (:method target-exists? (self)
     t)
-  (:method oracle-value (self)
+  (:method oracle-answer (self)
     (version-major-version (asdf-system-version name)))
   (:method target-node-label (self)
     (fmt "system major version ~a" name)))
@@ -263,18 +265,20 @@ recorded is simply nil."))
     (uiop:symbol-call :ql-dist :version dist)))
 
 (defclass dist-version-oracle (oracle)
-  ((key :initarg :name
-        :reader dist-version-oracle.name
-        :type string))
+  ((question :initarg :name
+             :reader dist-version-oracle.name
+             :type string))
   (:documentation "An oracle that reports the current version of a
   Quicklisp dist.
 
-By default this is the Quicklisp dist itself."))
+By default this is the Quicklisp dist itself.")
+  (:default-initargs
+   :name quicklisp))
 
-(defmethods dist-version-oracle (self (name key))
+(defmethods dist-version-oracle (self (name question))
   (:method target-exists? (self)
     (dist-exists? name))
-  (:method oracle-value (self)
+  (:method oracle-answer (self)
     (ql-dist-version name))
   (:method target= (self (other dist-version-oracle))
     (equal name (dist-version-oracle.name other))))
@@ -291,9 +295,9 @@ By default this is the Quicklisp dist itself."))
 ;;; portable?
 
 (defclass feature-oracle (oracle)
-  ((key :initarg :feature
-        :type keyword
-        :reader feature-oracle.feature))
+  ((question :initarg :feature
+             :type keyword
+             :reader feature-oracle.feature))
   (:documentation "An oracle that wraps whether a particular keyword
   is present in `*features*'."))
 
@@ -301,10 +305,10 @@ By default this is the Quicklisp dist itself."))
   (make 'feature-oracle
         :feature (assure keyword feature)))
 
-(defmethods feature-oracle (self (feature key))
+(defmethods feature-oracle (self (feature question))
   (:method target-exists? (self)
     (featurep feature))
-  (:method oracle-value (self)
+  (:method oracle-answer (self)
     t)
   (:method target= (self (other feature-oracle))
     (eql feature (feature-oracle.feature other))))
@@ -314,23 +318,23 @@ By default this is the Quicklisp dist itself."))
 
 ;;; For easy extension.
 (defclass function-oracle (oracle)
-  ((key :initarg :function
-        :type delayed-symbol
-        :reader function-oracle.delayed-symbol)
+  ((question :initarg :function
+             :type delayed-symbol
+             :reader function-oracle.delayed-symbol)
    (args
     :initarg :args
     :type list
     :reader function-oracle.args))
   (:documentation "An oracle for a user-supplied function.
 
-The function must be supplied by name."))
+The function must be supplied as a symbol."))
 
-(defmethods function-oracle (self (fn key) args)
+(defmethods function-oracle (self (fn question) args)
   (:method target-exists? (self)
     (fboundp
      (ignoring overlord-error
        (force-symbol fn))))
-  (:method oracle-value (self)
+  (:method oracle-answer (self)
     (apply (force-symbol fn) args)))
 
 (defun function-oracle (function-name &rest args)
