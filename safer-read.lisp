36e136765001588f3aaa0a1cadc34f34fd5c8263
Paul M. Rodriguez <pmr@ruricolist.com>
2019-01-03 13:59:17 -0600

    Use a global lexical.

--- safer-read.lisp
+++ safer-read.lisp
@@ -1,5 +1,5 @@
 (defpackage :overlord/safer-read
-  (:use :cl)
+  (:use :cl :serapeum)
   (:export :safer-read-from-string :safer-read)
   (:documentation "Safer variants of read and read-from-string."))
 
@@ -10,26 +10,27 @@
 (defparameter *safer-read-from-string-blacklist*
   '(#\# #+(or) #\: #\|))
 
-(let ((rt (copy-readtable nil)))
-  (defun safer-reader-error (stream closech)
-    (declare (ignore stream closech))
-    (error "safer-read-from-string failure"))
+(def rt (copy-readtable nil))
 
-  (dolist (c *safer-read-from-string-blacklist*)
-    (set-macro-character
-     c #'safer-reader-error nil rt))
+(defun safer-reader-error (stream closech)
+  (declare (ignore stream closech))
+  (error "safer-read-from-string failure"))
 
-  (defun safer-read-from-string (s &optional fail)
-    (if (stringp s)
-        (with-input-from-string (s s)
-          (safer-read s fail))
-        fail))
+(dolist (c *safer-read-from-string-blacklist*)
+  (set-macro-character
+   c #'safer-reader-error nil rt))
 
-  (defun safer-read (s &optional fail)
-    (let ((*readtable* rt) *read-eval*)
-      (handler-bind
-          ((error (lambda (condition)
-                    (declare (ignore condition))
-                    (return-from
-                     safer-read fail))))
-        (read s)))))
+(defun safer-read-from-string (s &optional fail)
+  (if (stringp s)
+      (with-input-from-string (s s)
+        (safer-read s fail))
+      fail))
+
+(defun safer-read (s &optional fail)
+  (let ((*readtable* rt) *read-eval*)
+    (handler-bind
+        ((error (lambda (condition)
+                  (declare (ignore condition))
+                  (return-from
+                   safer-read fail))))
+      (read s))))
