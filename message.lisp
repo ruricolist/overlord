6e954c4e681689c9fce2a3487ec4a722d11b2e8d
Paul M. Rodriguez <pmr@ruricolist.com>
2019-02-28 19:52:35 -0600

    Do some basic static sanity checking on message.

--- message.lisp
+++ message.lisp
@@ -1,15 +1,14 @@
 (defpackage :overlord/message
   (:use :cl :alexandria :serapeum
     :overlord/global-state)
-  (:import-from :overlord/types :overlord-condition)
+  (:import-from :overlord/types :error*)
+  (:import-from :cl-ppcre :all-matches-as-strings)
+  (:shadowing-import-from :cl-ppcre :scan)
   (:export
    :overlord-message
    :message
    :*message-stream*))
 (in-package :overlord/message)
-
-;;; TODO More logging levels? E.g. a level that just prints dots
-;;; instead of messages.
 
 (define-global-state *message-stream*
     (make-synonym-stream '*error-output*)
@@ -18,12 +17,34 @@
 (defun message (control &rest args)
   (let* ((stream *message-stream*)
          (control
-           (if (stringp control)
+           (if (and (stringp control)
+                    (string$= "." control))
                (string-right-trim "." control)
                control)))
     (format stream "~&[Overlord] ~?~%" control args)))
 
 (define-compiler-macro message (&whole call format-control &rest format-arguments)
   (if (not (stringp format-control)) call
-      (let ((format-control (string-right-trim "." format-control)))
-        `(message (formatter ,format-control) ,@format-arguments))))
+      (progn
+        (sanity-check-message-args format-control format-arguments)
+        (let ((format-control (string-right-trim "." format-control)))
+          `(message (formatter ,format-control) ,@format-arguments)))))
+
+(defun sanity-check-message-args (format-control format-arguments)
+  "Do some basic sanity-checking with format-control and format-arguments."
+  (when (stringp format-control)
+    (when-let (required (guess-arg-count format-control))
+      (let ((provided (length format-arguments)))
+        (unless (= required provided)
+          (error* "Message format string requires ~d argument~:p, but ~d ~:*~[were~;was~:;were~] provided."
+                  required
+                  provided))))))
+
+(defun guess-arg-count (format-string)
+  "When possible, Guess the number of arguments required by FORMAT-STRING."
+  (let ((directives (extract-directives format-string)))
+    (when (every (op (scan "~[a-zA-Z]" _)) directives)
+      (length directives))))
+
+(defun extract-directives (format-string)
+  (all-matches-as-strings "(~.)" format-string))
